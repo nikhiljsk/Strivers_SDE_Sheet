c3274ac568b4d37998ef1ef92bc75c73a70c5382
nikhiljsk <nikhiljsk98@gmail.com>
2023-01-23 20:08:50 +0530

    Day-10

---
+++ 6_Linked_List_II/6.5_Find_the_starting_point_of_the_loop_of_linked_list.go
@@ -0,0 +1,38 @@
+// Approach 1
+// O(N), O(N)
+// Use a hashmap to store the node address, if found while iterating return else nil
+
+// Approach 2
+// O(N), O(1)
+// Refer Notion for intuition or
+// https://www.youtube.com/watch?v=QfbOhn0WZ88&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=38&ab_channel=takeUforward
+func detectCycle(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+	t, h := head, head
+	var found bool
+	for h != nil && h.Next != nil {
+		h = h.Next.Next
+		t = t.Next
+		if t == h {
+			found = true // Collided, so cycle exists
+			break
+		}
+	}
+	if !found {
+		return nil
+	}
+
+	// Find the start point
+	t = head
+	for t != h {
+		t = t.Next
+		h = h.Next
+		if t == h {
+			return t
+		}
+	}
+	return t
+
+}
